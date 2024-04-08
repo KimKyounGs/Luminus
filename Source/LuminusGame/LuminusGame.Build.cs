f3b407b2b0183cea3cc1e4b1204cc0a72d07738c
KimKyounGs <77844251+KimKyounGs@users.noreply.github.com>
2024-04-08 20:37:19 +0900

    Initial

---
+++ Source/LuminusGame/LuminusGame.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class LuminusGame : ModuleRules
+{
+	public LuminusGame(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
---
+++ Source/LuminusGame/LuminusGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LuminusGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALuminusGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALuminusGameGameMode();
+};
+
+
+
