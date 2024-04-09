ac6e10bd51911fcda02dedb139dd3aea74ac5943
KimKyounGs <77844251+KimKyounGs@users.noreply.github.com>
2024-04-09 21:23:10 +0900

    7강 러프니스

---
+++ Source/LuminusGame/Character/MainCharacter.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MainCharacter.generated.h"
+
+UCLASS()
+class LUMINUSGAME_API AMainCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+private:
+
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	AMainCharacter();
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
