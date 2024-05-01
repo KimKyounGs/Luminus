f997713e0f445af6f82f9357f0d2c34aef5b479d
KimKyounGs <77844251+KimKyounGs@users.noreply.github.com>
2024-05-01 12:43:30 +0900

    다이얼로그

---
+++ Source/LuminusGame/Dialogue/DialogueManager.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DialogueManager.h"
+
+DialogueManager::DialogueManager()
+{
+
+}
+
+DialogueManager::~DialogueManager()
+{
+
+}
